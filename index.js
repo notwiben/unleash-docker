8c2074a4107c5dbf2dc44f5b0207101718e3d487
wibenson <wibenson@sirclo.co.id>
2020-03-12 20:11:10 +0700

    add google auth handler

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const unleash = require('unleash-server');
+// const unleash = require('../lib/server-impl.js');
 
-let options = {};
+const enableGoogleOauth = require('./google-auth-hook');
 
-unleash.start(options);
+unleash
+    .start({
+        databaseUrl: process.env.DATABASE_URL,
+        secret: process.env.SESSION_SECRET,
+        adminAuthentication: 'custom',
+        preRouterHook: enableGoogleOauth,
+    })
+    .then(server => {
+        console.log(
+            `Unleash started on http://localhost:${server.app.get('port')}`
+        );
+    });
